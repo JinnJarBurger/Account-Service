2d37085ade860cd6c5a894eb7d6934acc5ffaee5
Adnan <adnanhossain274@gmail.com>
2022-12-24 17:40:52 +0600

    Role Added

--- src/account/model/User.java
+++ src/account/model/User.java
@@ -8,6 +8,10 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+import static account.model.Role.ROLE_ADMINISTRATOR;
 
 /**
  * @author adnan
@@ -17,7 +21,6 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @ToString
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @Entity
 @Table(name = "user")
@@ -43,8 +46,25 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public User() {
+        roles = new HashSet<>();
+    }
+
     @JsonIgnore
     public boolean isNew() {
         return getId() == null || getId() == 0;
+    }
+
+    @JsonIgnore
+    public boolean isAdmin() {
+        return !getRoles().isEmpty() && getRoles().stream().anyMatch(role -> role.getName().equals(ROLE_ADMINISTRATOR));
     }
 }
--- src/account/model/UserDetailsImpl.java
+++ src/account/model/UserDetailsImpl.java
@@ -12,12 +12,10 @@ import java.util.Collections;
  */
 public class UserDetailsImpl implements UserDetails {
 
-    private final String email;
-    private final String password;
+    private final User user;
 
     public UserDetailsImpl(User user) {
-        this.email = user.getEmail();
-        this.password = user.getPassword();
+        this.user = user;
     }
 
     @Override
@@ -27,12 +25,12 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getPassword() {
-        return this.password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return this.email;
+        return user.getEmail();
     }
 
     @Override
